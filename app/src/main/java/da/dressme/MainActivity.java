1557d669df1ce1bd8b67c6b9846b340b27457da1
huang <huang.daniel.1996@gmail.com>
2018-11-10 01:06:29 -0500

    Main activity button should go to camera activity

--- app/src/main/java/da/dressme/MainActivity.java
+++ app/src/main/java/da/dressme/MainActivity.java
@@ -1,12 +1,25 @@
 package da.dressme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        cameraButton = (Button) findViewById(R.id.open_camera_button);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
     }
 }
